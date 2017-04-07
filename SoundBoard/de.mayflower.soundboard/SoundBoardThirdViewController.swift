799de3eb053c54356383d260e121d48d74dcee1c
christopherstock <email@christopherstock.de>
2017-04-07 12:27:24 +0200

    Added third screen class and resources.

---
+++ SoundBoard/de.mayflower.soundboard/SoundBoardThirdViewController.swift
@@ -0,0 +1,31 @@
+
+    import UIKit
+
+    /*******************************************************************************************************************
+    *   The third example view controller.
+    *
+    *   @author  Christopher Stock
+    *   @version 0.0.1
+    *******************************************************************************************************************/
+    class ThirdViewController: UIViewController {
+    
+        /***************************************************************************************************************
+        *   Being invoked when the view did load.
+        ***************************************************************************************************************/
+        override func viewDidLoad()
+        {
+            super.viewDidLoad()
+        
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+    
+        /***************************************************************************************************************
+        *   Being invoked when a memory warning is raised.
+        ***************************************************************************************************************/
+        override func didReceiveMemoryWarning()
+        {
+            super.didReceiveMemoryWarning()
+        
+            // Dispose of any resources that can be recreated.
+        }
+    }
